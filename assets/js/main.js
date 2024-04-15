7bdd0c3cc6609db048a9b45d5b27e771cf5ae7f4
vijay227799 <121456423+vijay227799@users.noreply.github.com>
2024-04-16 04:21:23 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,7 +84,7 @@ const sendEmail=(e)=>{
             },5000)
            },(error)=>{
                alert('OOPS! SOMETHING HAS FAILED....',error)
-           })
+           })                                                                                          
 
            //To clear the input field
            contactName.value=''
@@ -153,7 +153,26 @@ themeButton.addEventListener('click', () => {
 })
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add(bg-header) 
+                       : header.classList.remove(bg-header)
+}
+window.addEventListener('scroll', scrollHeader)
 
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+/*=============== SCROLL REVEAL ANIMATION SIMILAR TO PRESENTATION===============*/
 
+const sr=ScrollReveal({
+    origin:'top',
+    distance:'60px',
+    duration:250,
+    delay:400,
+})
+
+sr.reveal('.home__data,.projects__container,.footer__container')
+sr.reveal('.home__info div',{delay:600,origin:'bottom',interval:100})
+sr.reveal('.qualification__content,.services__card',{interval:100})
+sr.reveal('.skills__content:nth-child(1),.contact__content:nth-child(1)',{original:'left'})
+sr.reveal('.skills__content:nth-child(2),.contact__content:nth-chiled(2)',{origin:'left'})
